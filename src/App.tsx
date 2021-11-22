d2f92b4c2ecaf621c7d234ddade74430eb77467f
Anna Koehler <anna@foundrymakes.com>
2021-11-21 21:01:08 -0600

    changes component import style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
-import Sidebar from "./components/Sidebar";
-import Leftmenu from "./components/menus/Leftmenu";
-import Rightmenu from "./components/menus/Rightmenu";
+import {Sidebar} from "./components/Sidebar";
+import {Leftmenu} from "./components/menus/Leftmenu";
+import {Rightmenu} from "./components/menus/Rightmenu";
 import Main from "./components/Main";
 import Footer from "./components/footer/Footer";
 import styles from "./App.module.css";
