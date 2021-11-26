58446ab755158a10df77d875ee8c8f869ad0288c
Anna Koehler <anna@foundrymakes.com>
2021-11-26 12:08:12 -0600

    mobile navbar display

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import styles from "./Navbar.module.css";
+import {useWindowDimensions} from "../../hooks/useWindowDimensions";
+import {faBars} from "@fortawesome/free-solid-svg-icons/faBars";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function Navbar() {
+// function FontAwesomeIcon(props: { icon: any, size: string, className: string }) {
+//     return null;
+// }
+
+const Navbar = () => {
+    const {width} = useWindowDimensions();
+
+    //if on mobile, display hamburger menu
+    const getMobileMenu = () => {
+        if (width <= 768) {
+            return (
+                <FontAwesomeIcon icon={faBars} size="lg" className={styles.navMobileMenu}/>
+            );
+        }
+        return null;
+    }
     return (
-        <nav className={styles.navbar}>Nav</nav>
+        <nav className={styles.navbar}>
+            {getMobileMenu()}
+            <strong>SuperForum</strong>
+        </nav>
     );
 }
-
 export default Navbar;
